e4b497086a78b656bda7a71ac3fdc1158c7f9d5a
Reset <Reset@NB-004245.cwinet.local>
2020-03-07 15:47:37 -0300

    criação classe calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+
+    int somar(int valorUm, int valorDois) {
+        return valorUm + valorDois;
+    }
+
+    int subtracao(int valorUm, int valorDois) {
+        return valorUm - valorDois;
+    }
+
+    int multiplicacao(int valorUm, int valorDois) {
+        return valorUm * valorDois;
+    }
+
+    int divisao(int valorUm, int valorDois) {
+        return valorUm / valorDois;
+    }
+}
--- src/app.java
+++ src/app.java
@@ -1,7 +1,19 @@
 public class app {
 
     public static void main (String[] args) {
-        System.out.println("Heloo World");
+
+        Calculadora calculadoraDeIdades = new Calculadora();
+        int somaDasIdades = calculadoraDeIdades.somar(33,34);
+        System.out.println("Soma das idades: " + somaDasIdades);
+
+        int subtracaoDasIdades = calculadoraDeIdades.subtracao(33,34);
+        System.out.println("Subtração das idades: " + subtracaoDasIdades);
+
+        int multiplicacaoDasIdades = calculadoraDeIdades.multiplicacao(33,34);
+        System.out.println("Multiplicação das idades: " + multiplicacaoDasIdades);
+
+        int divisaoDasIdades = calculadoraDeIdades.divisao(33,34);
+        System.out.println("Divisão das idades: " + divisaoDasIdades);
 
     }
 }
