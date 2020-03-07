3ee9fa7f43f7f4e43ac53d25e8b658a1c7f83fb7
Reset <Reset@NB-004245.cwinet.local>
2020-03-07 12:13:07 -0300

    removendo mensagens estranhas

--- src/app.java
+++ src/app.java
@@ -2,8 +2,6 @@ public class app {
 
     public static void main (String[] args) {
         System.out.println("Heloo World");
-        System.out.println("outras coisas");
-        System.out.println("sem criatividade");
 
     }
 }
