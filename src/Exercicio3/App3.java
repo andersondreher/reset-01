a6f0c2059e8a6085b55f2f6fa48874f0435e08fe
Anderson <eng.andersondreher@gmail.com>
2020-03-08 23:15:19 -0300

    exercicio 3 da aula 1

--- src/Exercicio3/App3.java
+++ src/Exercicio3/App3.java
@@ -7,29 +7,29 @@ public class App3 {
         Aluno anderson = new Aluno("Anderson Dreher", 3.5);
 
         System.out.println("Nome do aluno: " + anderson.nome);
-        System.out.println("Com nota final: " + anderson.nota);
-        System.out.println("Está aprovado? " + anderson.alunoAprovado());
+        System.out.println("Nota final: " + anderson.nota);
+        System.out.println("O aluno foi aprovado? " + anderson.alunoAprovado());
         System.out.println("--------------------");
 
         Aluno monica = new Aluno("Mônica Streit", 9.5);
 
         System.out.println("Nome do aluno: " + monica.nome);
-        System.out.println("Com nota final: " + monica.nota);
-        System.out.println("Está aprovado? " + monica.alunoAprovado());
+        System.out.println("Nota final: " + monica.nota);
+        System.out.println("O aluno foi aprovado? " + monica.alunoAprovado());
         System.out.println("--------------------");
 
         Aluno marli = new Aluno("Marli Dreher", 9.9);
 
         System.out.println("Nome do aluno: " + marli.nome);
-        System.out.println("Com nota final: " + marli.nota);
-        System.out.println("Está aprovado? " + marli.alunoAprovado());
+        System.out.println("Nota final: " + marli.nota);
+        System.out.println("O aluno foi aprovado? " + marli.alunoAprovado());
         System.out.println("--------------------");
 
         Aluno jose = new Aluno("José Dreher", 9.4);
 
         System.out.println("Nome do aluno: " + jose.nome);
-        System.out.println("Com nota final: " + jose.nota);
-        System.out.println("Está aprovado? " + jose.alunoAprovado());
+        System.out.println("Nota final: " + jose.nota);
+        System.out.println("O aluno foi aprovado? " + jose.alunoAprovado());
     }
 
 }
