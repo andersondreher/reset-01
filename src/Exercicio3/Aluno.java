82ccd74818db858b0535e108ece24700dbedeb5f
Anderson <eng.andersondreher@gmail.com>
2020-03-08 22:53:11 -0300

    exercicios da aula 1

---
+++ src/Exercicio3/Aluno.java
@@ -0,0 +1,16 @@
+package Exercicio3;
+
+public class Aluno {
+
+    String nome;
+    double nota;
+
+    public Aluno (String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    boolean alunoAprovado(){
+        return nota >= 7.0;
+    }
+}
--- src/Exercicio3/App3.java
+++ src/Exercicio3/App3.java
@@ -4,16 +4,32 @@ public class App3 {
 
     public static void main(String[] args) {
 
-        NomeAluno nomeDeAluno = new NomeAluno();
-        String identificacao = nomeDeAluno.nomeDoAluno("Anderson");
-        System.out.println("Nome do Aluno : " + identificacao);
+        Aluno anderson = new Aluno("Anderson Dreher", 3.5);
 
-        float notaFinalDoAluno = nomeDeAluno.notaDoAluno(9.0);
-        System.out.println("Nota do Aluno: " + notaFinalDoAluno);
+        System.out.println("Nome do aluno: " + anderson.nome);
+        System.out.println("Com nota final: " + anderson.nota);
+        System.out.println("Está aprovado? " + anderson.alunoAprovado());
+        System.out.println("--------------------");
 
-        boolean alunoAprovado = nomeDeAluno.menorQue(7);
-        System.out.println("Aluno Aprovado: " + alun);
+        Aluno monica = new Aluno("Mônica Streit", 9.5);
 
+        System.out.println("Nome do aluno: " + monica.nome);
+        System.out.println("Com nota final: " + monica.nota);
+        System.out.println("Está aprovado? " + monica.alunoAprovado());
+        System.out.println("--------------------");
+
+        Aluno marli = new Aluno("Marli Dreher", 9.9);
+
+        System.out.println("Nome do aluno: " + marli.nome);
+        System.out.println("Com nota final: " + marli.nota);
+        System.out.println("Está aprovado? " + marli.alunoAprovado());
+        System.out.println("--------------------");
+
+        Aluno jose = new Aluno("José Dreher", 9.4);
+
+        System.out.println("Nome do aluno: " + jose.nome);
+        System.out.println("Com nota final: " + jose.nota);
+        System.out.println("Está aprovado? " + jose.alunoAprovado());
     }
 
 }
